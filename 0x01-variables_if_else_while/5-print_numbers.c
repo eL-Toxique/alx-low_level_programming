76e7d8c96f781941432e4dd6913770f54545663e
eL-Toxique <toxiqueel@gmail.com>
2023-02-17 11:47:07 -0800

    Prints all single digits base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- prints single digits base 10 (0-9)
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		printf("%d", x);
+		printf("\n");
+		return (0);
+	}
+}
