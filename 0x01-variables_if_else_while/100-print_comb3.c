6e114e5d0c5f67ef294f81f7b78e12bc364e84a7
eL-Toxique <toxiqueel@gmail.com>
2023-02-17 15:23:16 -0800

    Combination of 2 digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,16 +10,16 @@ int main(void)
 {
 	int digit1, digit2;
 
-	for (digit1 = 48; digit1 <= 56; digit1++)
+	for (digit1 = '0'; digit1 <= '9'; digit1++)
 	{
-		for (digit2 = 49; digit2 < 57; digit2++)
+		for (digit2 = '0'; digit2 < '9'; digit2++)
 		{
 			if (digit2 > digit1)
 			{
 				putchar(digit1);
 				putchar(digit2);
 
-				if (digit1 != 56 || digit2 != 57)
+				if (digit1 != '8' || (digit1 == '8' && digit2 != '9'))
 				{
 					putchar(',');
 					putchar(' ');
