768435eefa598bba0d2e27703110f86413d462bc
eL-Toxique <toxiqueel@gmail.com>
2023-02-17 10:08:36 -0800

    printing lowercase alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- printing lowercase alphabets
+ * Return: 0
+ */
+int main(void)
+{
+	char lo;
+
+	for (lo = 'a'; lo <= 'z'; lo++)
+	{
+		putchar(lo);
+	}
+	putchar('\n');
+	return (0);
+}
