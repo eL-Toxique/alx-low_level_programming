463600dffc99e937498a579d68c54b1e64e40a8f
eL-Toxique <toxiqueel@gmail.com>
2023-02-17 14:45:24 -0800

    Print all numbers of base 16

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - prints combination of 2 2 digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++);
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || != 99);
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,10 +8,10 @@ int main(void)
 	int num;
 	char letter;
 
-	for (num = 0; num < 10; num++)
-		putchar((num % 10) + '0');
+	for (num = 48; num < 58; num++)
+		putchar(num);
 
-	for (letter = 'a'; letter = 'f'; letter++)
+	for (letter = 'a'; letter <= 'f'; letter++)
 		putchar (letter);
 	putchar ('\n');
 	return (0);
