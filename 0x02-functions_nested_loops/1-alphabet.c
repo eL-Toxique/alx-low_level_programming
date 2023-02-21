d9c238910d5c191069f1888d81fba751718ed682
eL-Toxique <toxiqueel@gmail.com>
2023-02-21 03:25:05 -0800

    Printing lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet - print in lowercase all alphabets
+ */
+void print_alphabet(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+		_putchar(l);
+_putchar('\n');
+}
