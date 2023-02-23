eb3fe9b3dbce93fa2a88439e4cbb54cef97f4c5b
eL-Toxique <toxiqueel@gmail.com>
2023-02-23 06:57:31 -0800

    printing 0-9 but not 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints from 0-9
+ * does not print 2 and 4
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 10; x++)
+	{
+		if (x != 2 && x != 4)
+		{
+			_putchar((n % 10) + '0');
+		}
+	}
+	_putchar('\n');
+}
