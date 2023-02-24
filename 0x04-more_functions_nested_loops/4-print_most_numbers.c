99a0ffe95a8d0f4d3aa728351b18f56905f91656
eL-Toxique <toxiqueel@gmail.com>
2023-02-24 03:24:23 -0800

    Print 0-9 without 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,13 @@ void print_most_numbers(void)
 {
 	int x;
 
-	for (x = 0; x <= 10; x++)
+	for (x = 48; x <= 58; x++)
 	{
-		if (x != 2 && x != 4)
+		if ((x == 50) || (x == 52))
 		{
-			_putchar((x % 10) + '0');
+			continue;
 		}
+		putchar(x);
 	}
-	_putchar('\n');
+	putchar(10);
 }
