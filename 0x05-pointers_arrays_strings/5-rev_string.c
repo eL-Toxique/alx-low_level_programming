90095b46015414ea90d20a1ddd3e1ffec50749cd
eL-Toxique <toxiqueel@gmail.com>
2023-02-27 08:30:08 -0800

    Reverse of a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - reverse a strig
+ * @s:input string
+ * Return:reversed string
+ */
+void rev_string(char *s)
+{
+	char r = s[0];
+	int c = 0;
+	int i;
+
+	while (s[c] != '\0')
+	c++;
+
+	for (i = 0; i < c; i++)
+	{
+		c--;
+		r = s[i];
+		s[i] = s[c];
+		s[c] = r;
+	}
+}
