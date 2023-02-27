a9ff10b86016cfbddd6c8cea7018189eb0b4c865
eL-Toxique <toxiqueel@gmail.com>
2023-02-27 09:03:35 -0800

    Print half a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts_half - print half of a string
+ * if odd n=(length_of_the_string - 1)/2
+ * @str:input
+ * Return:half of the input
+ */
+void puts_half(char *str)
+{
+	int u, l, r;
+
+	r = 0;
+
+	for (u = 0; str[u] != '\0'; u++)
+		r++;
+	l = (r / 2);
+	if ((r % 2) == 1)
+		l + ((r + 1) / 2);
+	for (u = l; str[u] != '\0'; u++)
+		_putchar(str[u]);
+
+	_putchar('\n');
+}
