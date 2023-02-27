d2f65ae7edf3d6f90f550e3a54e9f3d47e26718d
eL-Toxique <toxiqueel@gmail.com>
2023-02-27 11:56:55 -0800

    Copying string from src

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_array - print n elements of an  array of integer
+ * @a:array name
+ * @n:number of elements
+ * Return:elements
+ */
+void print_array(int *a, int n)
+{
+	int x;
+
+	for (x = 0; x < (n - 1); x++)
+	{
+		printf("%d", a[x]);
+	}
+	if (x == (n - 1))
+	{
+	printf("%d", a[n - 1]);
+	}
+	printf("\n");
+}
