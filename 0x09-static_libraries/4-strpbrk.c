a4f46d92364b95611b2291c1f2a4aa155287f1ac
eL-Toxique <toxiqueel@gmail.com>
2023-03-10 17:47:48 -0800

    Creating a static library

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strpbrk- searches a string for any set of bytes
+ * @s:occurence being searched
+ * @accept:string being searched
+ *
+ * Return:string
+ */
+char *_strpbrk(char *s, char *accept)
+{
+        int x;
+
+        while (*s)
+        {
+                for (x = 0; accept[x]; x++)
+                {
+                        if (*s == accept[x])
+                                return (s);
+                }
+                s++;
+        }
+        return ('\0');
+}
