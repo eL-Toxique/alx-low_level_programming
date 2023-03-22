de80079555dccc46d177484b5ad96adc57c65da5
eL-Toxique <toxiqueel@gmail.com>
2023-03-22 00:17:23 -0700

    first commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - initializes a variable type struct dog
+ * @d: pinter to struct dog
+ * @name: element 1
+ * @age: element 2
+ * @owner:
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
