711cc0276f59cafb80a633569f5b7d4b37e9fa19
eL-Toxique <toxiqueel@gmail.com>
2023-04-03 09:37:00 -0700

    description correction

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,10 +3,10 @@
 
 /**
  * init_dog - initializes a variable type struct dog
- * @d: pinter to struct dog
+ * @d: pointer to struct dog
  * @name: element 1
  * @age: element 2
- * @owner:
+ * @owner: element 3
  *
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
