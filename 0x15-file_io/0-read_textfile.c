afe389a2cc94fe3b60ba19cbc61f6cf150fd9bca
eL-Toxique <toxiqueel@gmail.com>
2023-07-10 08:48:42 -0700

    code correction

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,3 +11,43 @@
  * @letters: number of letter it should read and print
  * Return: the actual number of letters it could read and print
  */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buf;
+	ssize_t s, d;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	{
+		if (!buf)
+			return (0);
+	}
+
+	s = read(fd, buf, letters);
+	if (s == -1)
+	{
+		free(buf);
+		return (0);
+	}
+
+	buf[s] = '\0';
+
+	close(fd);
+
+	d = write(STDOUT_FILENO, buf, s);
+	if (d == -1)
+	{
+		free(buf);
+		return (0);
+	}
+
+	free(buf);
+	return (d);
+}
